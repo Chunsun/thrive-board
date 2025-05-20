ffbbc550f6651ff862d6e824737b4cc5a6ed7ae9
DESKTOP-0H9MUO0\Yo <cycbpm@gmail.com>
2025-05-20 08:54:49 +0800

    chore: github page settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/thrive-board/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
